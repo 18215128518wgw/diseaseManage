5a8aaadb8e5ee23c42652dbde6781cf6869daad1
wgw <2537524575@qq.com>
2020-03-06 16:54:02 +0800

    使用thymeleaf

--- src/main/java/cn/itcast/user/controller/UserController.java
+++ src/main/java/cn/itcast/user/controller/UserController.java
@@ -4,10 +4,13 @@ import cn.itcast.user.pojo.User;
 import cn.itcast.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("user")
@@ -16,6 +19,11 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    /**
+     * 测试方法
+     * @param id
+     * @return
+     */
     @GetMapping("{id}")
     @ResponseBody
     public User queryUserById(@PathVariable("id")int id) {
@@ -30,11 +38,21 @@ public class UserController {
         return "userController";
     }
 
-    @GetMapping("hello")
-    @ResponseBody
-    public String hello() {
-        return "hello";
-    }
 
+    /**
+     *查询全部信息
+     * @param model
+     * @return
+     */
+    @GetMapping("/")
+    public String all(ModelMap model) {
+        // 查询用户
+        List<User> users = this.userService.queryAll();
+        //System.out.println(users);
+        // 放入模型
+        model.addAttribute("users", users);
+        // 返回模板名称（就是classpath:/templates/目录下的html文件名）
+        return "users";
+    }
 
 }
--- src/main/java/cn/itcast/user/service/UserService.java
+++ src/main/java/cn/itcast/user/service/UserService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
@@ -14,6 +16,10 @@ public class UserService {
 
     public User queryUserById(int id) {
         return this.userMapper.selectByPrimaryKey(id);
+    }
+
+    public List<User> queryAll() {
+        return this.userMapper.selectAll();
     }
 
     @Transactional
