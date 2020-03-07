a9ccdcd30543795886d2f86f4aa2d3a803b90108
wgw <2537524575@qq.com>
2020-03-07 20:49:34 +0800

    完善功能：打卡的条件限制

--- src/main/java/cn/itcast/user/controller/UploadController.java
+++ src/main/java/cn/itcast/user/controller/UploadController.java
@@ -8,36 +8,57 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Calendar;
 import java.util.Date;
 
 @Controller
-@RequestMapping("upload")
+@RequestMapping("index")
 public class UploadController {
 
     @Autowired
     private StudentService studentService;
 
-    @GetMapping("student")
+    /**
+     * 返回页面视图
+     * @param model
+     * @return
+     */
+    @GetMapping("index")
     public String StudentUpload(Model model){
 
         model.addAttribute("today", new Date());
         return "student";
+
     }
 
+    /**
+     * 表单提交的目标方法，判断条件是否允许，为真则向数据库写入数据
+     * @param model
+     * @param student
+     * @param httpServletResponse
+     * @return   打卡视图
+     * @throws Exception
+     */
     @GetMapping("info")
     public String StudentInfo(Model model, Student student, HttpServletResponse httpServletResponse) throws Exception{
 
         System.out.println(student);
 
-        if(studentService.InsertStudentInfo(student)) {
-            //model.addAttribute("msg", "success");
+        //成功表示当日没有填写，并且时间未超过20点
+        if(studentService.checkStudentByCode(student.getCode(), student.getDate()) && getHour(new Date()) < 20){
+            //判断插入到数据表中是否成果
+            if(studentService.InsertStudentInfo(student)) {
+                httpServletResponse.setContentType("text/html;charset=utf-8");
+                httpServletResponse.getWriter().write("<script>alert('今日情况已上报成功！');</script>");
+                httpServletResponse.getWriter().flush();
+            }else {
+                httpServletResponse.setContentType("text/html;charset=utf-8");
+                httpServletResponse.getWriter().write( "<script>alert('上报失败，请重新上报！');</script>");
+                httpServletResponse.getWriter().flush();
+            }
+        }else {
             httpServletResponse.setContentType("text/html;charset=utf-8");
-            httpServletResponse.getWriter().write("<script>alert('今日情况已上报成功！');</script>");
-            httpServletResponse.getWriter().flush();
-        } else {
-            //model.addAttribute("msg", "fail");
-            httpServletResponse.setContentType("text/html;charset=utf-8");
-            httpServletResponse.getWriter().write( "<script>alert('上报失败，请重新上报！');</script>");
+            httpServletResponse.getWriter().write( "<script>alert('打卡时间超过规定时间，或者您今日已打卡！');</script>");
             httpServletResponse.getWriter().flush();
         }
 
@@ -46,6 +67,18 @@ public class UploadController {
     }
 
 
+    /**
+     * 功能描述：返回小时
+     *
+     * @param date
+     *
+     * @return 返回小时
+     */
+    public static int getHour(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.HOUR_OF_DAY);
+    }
 
 
 }
--- src/main/java/cn/itcast/user/service/StudentService.java
+++ src/main/java/cn/itcast/user/service/StudentService.java
@@ -5,14 +5,78 @@ import cn.itcast.user.pojo.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 @Service
 public class StudentService {
 
     @Autowired(required = false)
     private StudentMapper studentMapper;
+    private Student student;
 
     public boolean InsertStudentInfo(Student student) {
         return this.studentMapper.insert(student) == 1;
+    }
+
+    /**
+     * 根据code找到所有打卡记录，遍历记录，判断当天是否打卡
+     * @param code
+     * @param date
+     * @return
+     */
+    public boolean checkStudentByCode(String code, String date) {
+        Student s = new Student();
+        s.setCode(code);
+        List<Student> r = studentMapper.select(s);
+
+        for (Student student : r) {
+            if(Integer.parseInt(student.getDate().substring(0,4)) == getYear(new Date()) && Integer.parseInt(student.getDate().substring(4,6)) == getMonth(new Date()) && Integer.parseInt(student.getDate().substring(7,9)) == getDay(new Date() )){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * 获取日期年份
+     * @param date 日期
+     * @returninteresting 返回年份
+     */
+    public static int getYear(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.YEAR);
+    }
+
+
+    /**
+     * 功能描述：返回月
+     *
+     * @param date
+     *            Date 日期
+     * @return 返回月份
+     */
+    public static int getMonth(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.MONTH) + 1;
+    }
+
+
+    /**
+     * 功能描述：返回日期
+     *
+     * @param date
+     *            Date 日期
+     * @return 返回日份
+     */
+    public static int getDay(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.DAY_OF_MONTH);
     }
 
 }
